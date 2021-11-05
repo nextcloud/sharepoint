8df680f142093599857028666ec7df65ecda4fb8
Arthur Schiwon <blizzz@arthur-schiwon.de>
2021-11-05 19:14:55 +0100

    adjust app to library changes
    - file content items are provided as Field
    - fetch lastResponse via event, since Requests::getHistory() was removed
    - getProperties() was removed, provide a list of known properties
    - adjust to changed auth mechanism
    - migrate NTLM auth mechanism
    
    Signed-off-by: Arthur Schiwon <blizzz@arthur-schiwon.de>

--- lib/Storage/Storage.php
+++ lib/Storage/Storage.php
@@ -30,7 +30,6 @@ use OC\Files\Storage\Common;
 use OCA\SharePoint\Client;
 use OCA\SharePoint\ClientFactory;
 use OCA\SharePoint\ContextsFactory;
-use OCA\SharePoint\Helper\RequestsWrapper;
 use OCA\SharePoint\NotFoundException;
 use OCP\Files\FileInfo;
 use OCP\ILogger;
@@ -557,7 +556,7 @@ class Storage extends Common {
 			&& $parameters['sharePointClientFactory'] instanceof ClientFactory) {
 			$spcFactory = $parameters['sharePointClientFactory'];
 		} else {
-			$spcFactory = new ClientFactory(new RequestsWrapper());
+			$spcFactory = new ClientFactory();
 		}
 		$this->spClient = $spcFactory->getClient(
 			$this->contextsFactory,
